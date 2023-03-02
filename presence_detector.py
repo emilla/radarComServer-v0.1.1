57d031de42f10c9d4d1b8e5b8e6419bab8f7bf71
Edgar Milla <edgar.milla#gmail.com>
2023-03-02 00:45:10 -0800

    undid changes

--- presence_detector.py
+++ presence_detector.py
@@ -27,11 +27,10 @@ class PresenceDetector(radar_module.XMModule):
         await self._value_matches(self, self.mode_selection, 0x400)
         print(f"Mode set to presence: {self.mode_selection.value:x}")
         self.range_start.value = 200
-        await asyncio.sleep(0.3)
-        self.range_length.value = 7000
-        await asyncio.sleep(0.3)
+        await self._value_matches(self, self.range_start, 200)
+        self.range_length.value = 5000
+        await self._value_matches(self, self.range_length, 5000)
         self.update_rate.value = 1000
-        await asyncio.sleep(0.3)
 
     async def start_detector(self, duration=60, func=None):
 
