37406b2123cb7fc86519efbc47513416b67d49e7
Edgar Milla <edgar.milla#gmail.com>
2023-03-01 22:32:43 -0800

    adding async to main

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from display import Display
 from presence_detector import PresenceDetector
+import asyncio
 
 display = Display(128, 64, 0x3C)
 
 
-def main():
+async def main():
     com_config = {
         'port': '/dev/ttyUSB0',
         'baudrate': 115200,
@@ -15,6 +16,7 @@ def main():
         'range_start': 5,
         'range_length': 4
     }
+
     detector = PresenceDetector(mod_config, com_config)
     print(detector.get_module_info())
     print(detector.get_module_status())
@@ -27,4 +29,4 @@ def main():
 # Press the green button in the gutter to run the script.
 
 if __name__ == '__main__':
-    main()
+    asyncio.run(main())
